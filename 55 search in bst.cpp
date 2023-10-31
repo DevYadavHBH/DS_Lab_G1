4a217f37671f0d907e927279df7ec97b9001e5fe
DevYadavHBH <113538731+DevYadavHBH@users.noreply.github.com>
2023-10-31 21:35:39 +0530

    Update 55 search in bst.cpp

--- 55 search in bst.cpp
+++ 55 search in bst.cpp
@@ -1,93 +1,93 @@
 #include <iostream>
 #include <queue>
-#include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
-class node{
+class node {
 public:
     int data;
-    node*left;
-    node*right;
+    node* left;
+    node* right;
 
-    node(int d){
-        this -> data = d;
-        this -> left = NULL;
-        this -> right = NULL;
+    node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-node* insertintobst(node* root , int d){
-    if(root == NULL){
-        root = new node(d);
-        return root;
+node* insertIntoBST(node* root, int d) {
+    if (root == NULL) {
+        return new node(d);
     }
 
-    if(d > root -> data){
-        root->right = insertintobst(root -> right, d);
-    }
-    else{
-        root -> left = insertintobst(root -> left, d);
+    if (d > root->data) {
+        root->right = insertIntoBST(root->right, d);
+    } else {
+        root->left = insertIntoBST(root->left, d);
     }
 
     return root;
 }
 
-void levelorder(node* root){
+void levelOrder(node* root) {
+    if (root == NULL) {
+        return;
+    }
+
     queue<node*> q;
     q.push(root);
-    q.push(NULL);
-    while(!q.empty()){
-        node*temp = q.front();
-        q.pop();
-        if(temp == NULL){
-            cout << endl;
-            if(!q.empty()){
-                q.push(NULL);
-            }
-        }
-        else{
-            cout << temp -> data << " " ;
-            if(temp -> left){
-                q.push(temp -> left);
 
-            }
-            if(temp -> right){
-                q.push(temp -> right);
-            }
+    while (!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+
+        if (temp->left) {
+            q.push(temp->left);
+        }
+        if (temp->right) {
+            q.push(temp->right);
         }
     }
 }
 
 bool search(node* root, int target) {
     if (root == nullptr) {
-        
         return false;
     }
 
     if (root->data == target) {
-        
         return true;
-    } 
-    else if (target < root->data) {
-        
+    } else if (target < root->data) {
         return search(root->left, target);
-    } 
-    else {
-        
+    } else {
         return search(root->right, target);
     }
 }
-int main(){
-    node *root = NULL;
-    cout <<  " enter the data" << endl;
-    takeinput(root);
-    levelorder(root);
+
+node* takeInput() {
+    int data;
+    cin >> data;
+    node* root = NULL;
+    while (data != -1) {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+    return root;
+}
+
+int main() {
+    node* root = takeInput();
+    cout << "Level Order Traversal of the BST: ";
+    levelOrder(root);
+    cout << endl;
+
     int target = 4;
-   if (search(root, target)) {
+    if (search(root, target)) {
         cout << target << " found in the BST." << endl;
     } else {
         cout << target << " not found in the BST." << endl;
     }
 
+    return 0;
 }
