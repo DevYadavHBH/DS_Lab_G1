a1a5df561678ae60ca11634423208847ebf770de
DevYadavHBH <113538731+DevYadavHBH@users.noreply.github.com>
2023-10-31 21:29:39 +0530

    Update 54 creation of bst deletion.cpp

--- 54 creation of bst deletion.cpp
+++ 54 creation of bst deletion.cpp
@@ -1,115 +1,119 @@
 #include <iostream>
 #include <queue>
-#include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
-class node{
+class node {
 public:
     int data;
-    node*left;
-    node*right;
+    node* left;
+    node* right;
 
-    node(int d){
-        this -> data = d;
-        this -> left = NULL;
-        this -> right = NULL;
+    node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-node* insertintobst(node* root , int d){
-    if(root == NULL){
-        root = new node(d);
-        return root;
+node* insertIntoBST(node* root, int d) {
+    if (root == NULL) {
+        return new node(d);
     }
 
-    if(d > root -> data){
-        root->right = insertintobst(root -> right, d);
+    if (d > root->data) {
+        root->right = insertIntoBST(root->right, d);
     }
-    else{
-        root -> left = insertintobst(root -> left, d);
+    else {
+        root->left = insertIntoBST(root->left, d);
     }
 
     return root;
 }
 
-void levelorder(node* root){
+void levelOrder(node* root) {
+    if (root == NULL) {
+        return;
+    }
+
     queue<node*> q;
     q.push(root);
-    q.push(NULL);
-    while(!q.empty()){
-        node*temp = q.front();
-        q.pop();
-        if(temp == NULL){
-            cout << endl;
-            if(!q.empty()){
-                q.push(NULL);
-            }
-        }
-        else{
-            cout << temp -> data << " " ;
-            if(temp -> left){
-                q.push(temp -> left);
 
-            }
-            if(temp -> right){
-                q.push(temp -> right);
-            }
+    while (!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+
+        if (temp->left) {
+            q.push(temp->left);
+        }
+        if (temp->right) {
+            q.push(temp->right);
         }
     }
 }
-node* deletion(node*root, int d){
-    if(root == NULL){
+
+node* findMinimum(node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+node* deleteNode(node* root, int key) {
+    if (root == NULL) {
         return root;
     }
-    if(root -> data == d){
-        if(root -> left == NULL && root -> right == NULL){
-            delete root;
-            return NULL;
-        }
-        if(root -> left && !root -> right){
-            node* temp = root -> left;
-            delete root;
-            return temp;
-        }
-         if(!root -> left && root -> right){
-            node* temp = root -> right;
+
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+            node* temp = root->right;
             delete root;
             return temp;
         }
-         if(root -> left && root -> right){
-            int mini = minimum(root -> right);
-            root -> data = mini;
-            root -> right = deletion(root -> right, mini);
+        else if (root->right == NULL) {
+            node* temp = root->left;
+            delete root;
+            return temp;
         }
 
+        node* temp = findMinimum(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
     }
-    else if(root -> data > d){
-          root -> left = deletion(root -> left, d);
-          return root;
-    }
-    else{
-         root -> right = deletion(root -> right, d);
-         return root;
-    }
+    return root;
 }
-void takeinput(node* &root){
-    int data ;
+
+void takeInput(node*& root) {
+    int data;
     cin >> data;
-    while(data != -1){
-        root = insertintobst(root, data);
+    while (data != -1) {
+        root = insertIntoBST(root, data);
         cin >> data;
     }
 }
 
+int main() {
+    node* root = NULL;
+    cout << "Enter the data (-1 to end input):" << endl;
+    takeInput(root);
+    cout << "Level Order Traversal of the BST: ";
+    levelOrder(root);
+    cout << endl;
 
-int main(){
-    node *root = NULL;
-    cout <<  " enter the data" << endl;
-    takeinput(root);
-    levelorder(root);
-    node* temp = deletion(root, 3);
-    cout<<"after deleting"<<endl;
-    levelorder(temp);
+    int key;
+    cout << "Enter the node to delete: ";
+    cin >> key;
+    root = deleteNode(root, key);
 
+    cout << "BST after deleting node " << key << ": ";
+    levelOrder(root);
+    cout << endl;
+
+    return 0;
 }
