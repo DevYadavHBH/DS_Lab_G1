b7326df239290fc8b2cf831bff550a447da902ec
DevYadavHBH <113538731+DevYadavHBH@users.noreply.github.com>
2023-10-31 19:33:22 +0530

    Update 16  Link list insertion and deletion at beginning.cpp

--- 16  Link list insertion and deletion at beginning.cpp
+++ 16  Link list insertion and deletion at beginning.cpp
@@ -1,91 +1,100 @@
-#include <iostream >
+#include <iostream>
 using namespace std;
 
-class node{
-	public:
-	int data;
-	node* next;
-	node(int data){
-		this -> data = data;
-		this -> next = NULL;
-	}
-	 ~node() {
-        int value = this -> data;
-        //memory free
-        if(this->next != NULL) {
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
+
+    ~Node() {
+        // Destructor to release memory recursively for the linked list
+        cout << "Memory freed for node with data: " << data << endl;
+        if (next != nullptr) {
             delete next;
-            this->next = NULL;
         }
-        cout << "memory is free for node with data " << value << endl;
     }
 };
 
-
-void insertathead(node* &head, node* &tail,  int d){
-	    if(head == NULL) {
-	        node* temp = new node(d);
-	        head = temp;
-	        tail = temp;
-	    }
-	    else{
-	        node* temp = new node(d);
-	        temp -> next = head;
-	        head = temp;
-	    }
-}
-
-
-		
-void deleteNode(int position, node* & head) { 
-     if (head == NULL) {
-            cout << "List is empty. Nothing to delete." << endl;
-            return;
-        }
-    
-    if(position == 1) {
-        node* temp = head;
-        head = head -> next;
-        temp -> next = NULL;
-        delete temp;
+void insertAtHead(Node* &head, Node* &tail, int data) {
+    // Insert a new node at the head of the linked list
+    Node* newNode = new Node(data);
+    if (head == nullptr) {
+        head = tail = newNode;
+    } else {
+        newNode->next = head;
+        head = newNode;
     }
-    
-   
 }
 
-void print(node* &head){
-	node* temp = head;
-	while(temp != NULL){
-		cout << temp -> data << " ";
-		temp = temp -> next;
-	}
-	cout << endl;
+void deleteNode(int position, Node* &head) {
+    if (head == nullptr) {
+        cout << "List is empty. Nothing to delete." << endl;
+        return;
+    }
+
+    if (position == 1) {
+        Node* temp = head;
+        head = head->next;
+        temp->next = nullptr;
+        delete temp;
+    } else if (position > 1) {
+        int currentPos = 1;
+        Node* temp = head;
+        Node* prev = nullptr;
+
+        while (temp != nullptr && currentPos < position) {
+            prev = temp;
+            temp = temp->next;
+            currentPos++;
+        }
+
+        if (temp == nullptr) {
+            cout << "Invalid position to delete." << endl;
+        } else {
+            prev->next = temp->next;
+            temp->next = nullptr;
+            delete temp;
+        }
+    } else {
+        cout << "Invalid position to delete." << endl;
+    }
 }
+
+void print(Node* head) {
+    // Print the elements of the linked list
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main() {
-    node*head = NULL;
-    node*tail = NULL;
+    Node* head = nullptr;
+    Node* tail = nullptr;
 
-    
-    insertathead(head, tail, 3);
-    insertathead(head, tail, 2);
-    insertathead(head, tail, 1);
+    // Insert elements at the head
+    insertAtHead(head, tail, 3);
+    insertAtHead(head, tail, 2);
+    insertAtHead(head, tail, 1);
 
-    cout << "Linked List after insertions : " << endl;
+    cout << "Linked List after insertions: ";
     print(head);
 
-    
-    deleteNode(0, head);
+    // Delete a node at position 1 (head)
+    deleteNode(1, head);
 
-    cout << "Linked List after deletion: " << endl;
+    cout << "Linked List after deletion: ";
     print(head);
+
+    // Destructor for releasing memory
+    if (head != nullptr) {
+        delete head;
+        head = nullptr;
+    }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
